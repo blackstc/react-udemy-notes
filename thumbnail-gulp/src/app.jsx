31bc2e274cad81b7f3836bf1fe2aa61d5132c5f0
Blackstock <charlieblackstock@gmail.com>
2015-11-09 23:09:05 -0700

    added gulp comps for react

---
+++ thumbnail-gulp/src/app.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ThumbnailList = require('./thumbnail-list');
+
+var options = {
+  thumbnailData: [
+    {
+      title: "See tutorials",
+      number: 32,
+      header: 'Learn React',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      imageUrl: 'http://formatjs.io/img/react.svg'
+    },
+    {
+      title: "See tutorials",
+      number: 12,
+      header: 'Learn Gulp',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      imageUrl: 'https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png'
+    }
+  ]
+};
+
+// React, please render this class
+var element = React.createElement(ThumbnailList, options);
+
+// React, after you render this class, please place it in my body tag
+React.render(element, document.querySelector('.target'));
